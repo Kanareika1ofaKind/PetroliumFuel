bbcd546e1ebb10fd0f41b0a9b471b4024738f240
Sydney <proteimus@gmail.com>
2022-03-16 10:00:43 -0500

    add gh-pages

---
+++ script.js
@@ -0,0 +1,17 @@
+let VAT = 7;
+
+function pumpCalc() {
+    let pump = document.forms["gas"]["pump"].value;
+    let pricePLiter = document.forms["gas"][pump].value;
+    let liters = document.forms["gas"]["liters"].value;
+    let totalPrice = (pricePLiter * liters);
+    let valueTax = (totalPrice * (VAT / 100));
+
+    document.getElementById('petrolType').innerText = pump;
+    document.getElementById('pricePerLiter').innerText = '$' + Number(pricePLiter).toFixed(2);
+    document.getElementById('totalLiters').innerText = liters;
+    document.getElementById('VAT').innerText = VAT + '%' + ` ($${valueTax.toFixed(2)})`;
+    document.getElementById('totalPrice').innerText = '$' + (valueTax + totalPrice).toFixed(2);
+}
+
+pumpCalc()
